7f6f02c3e205b78033ef449828c22221a7cb37d2
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-10-15 17:52:14 -0400

    hash table implementation 2

--- nyc-coders-hash-table-3.js
+++ nyc-coders-hash-table-3.js
@@ -10,4 +10,45 @@ function hash(key, arrayLen) {
     // keeps it within arrayLen
     output = (output + value) % arrayLen
   }
+
+  return output
+}
+
+class HashTable {
+  constructor(buckets){
+    this.size = buckets || 100
+    this.array = Array(this.size).fill(null)
+  }
+
+  get(key){
+    const index = hash(key, this.size)
+    if (this.array[index] === null) return null
+    return this.array[index].val
+  }
+
+  set(key, val){
+    const index = hash(key, this.size)
+    this.array[index] = { key: key, val: val }
+    return this
+  }
+
+  keys() {
+    const allKeys = []
+    for (let i = 0; i < this.size; ++i){
+      if(this.array[i]) allKeys.push(this.array[i].key)
+    }
+
+    return allKeys
+  }
+
+  values() {
+    const allValues = [];
+    for (let i = 0; i < this.size; ++i){
+      if(this.array[i]) allValues.push(this.array[i].val)
+    }
+    return allValues
+  }
+
+
+
 }
