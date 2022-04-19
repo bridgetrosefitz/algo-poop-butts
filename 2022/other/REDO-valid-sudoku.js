aca6d39824b0933e10878e37f6413b4a8ecef086
bridgetrosefitz <bridgetrosefitz@gmail.com>
2022-04-19 20:39:27 +1000

    work in progress - valid sudoku

--- 2022/other/REDO-valid-sudoku.js
+++ 2022/other/REDO-valid-sudoku.js
@@ -1,3 +1,9 @@
+// https://leetcode.com/problems/binary-search/
+// Attempted 220415
+// No solution reached in 20mins
+
+// Solution from https://www.youtube.com/watch?v=TjFXEUCMqI8&ab_channel=NeetCode
+
 Input: board =
   [["5", "3", ".", ".", "7", ".", ".", ".", "."]
     , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
@@ -10,6 +16,17 @@ Input: board =
     , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
 Output: true
 
-const myset = new Set()
+const SudokuIsValid = (board) => {
+  cols = new Set()
+  // Haven't figured out how to make the equivalent of the Python below:
+  // cols = collections.defaultdict(set)
+  // rows = collections.defaultdict(set)
+  // squares = collections.defaultdict(set)
+
+}
+
+
+
+
 
 // https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Sudoku-by-L2G-20050714.svg/250px-Sudoku-by-L2G-20050714.svg.png
